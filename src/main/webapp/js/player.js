f5a713f9a39b8c7392ee1c6f71c8f50834e53a66
MokoMaracucho <desmoulin.federico@gmail.com>
2020-11-06 18:09:58 +0000

    Deplacer la cemra sur la scene : nouvelle camera

--- src/main/webapp/js/player.js
+++ src/main/webapp/js/player.js
@@ -6,11 +6,14 @@ Player = function(game, canvas) {
 };
 
 Player.prototype = {
-    _initCamera : function(scene, canvas) {
-        this.camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 5, -10), scene);
+	_initCamera : function(scene, canvas) {
+	    // On crée la caméra
+	    this.camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(-20, 5, 0), scene);
+	    
+	    // On demande à la caméra de regarder au point zéro de la scène
+	    this.camera.setTarget(BABYLON.Vector3.Zero());
 
-        this.camera.setTarget(BABYLON.Vector3.Zero());
-
-        this.camera.attachControl(canvas, true);
-    }
+	    // On affecte le mouvement de la caméra au canvas
+	    this.camera.attachControl(canvas, true);
+	}
 };
